106a76a0f33a42cc52af2291c4afeaa0de4da948
Sorarinu <c01143125b@edu.teu..jp>
2016-07-10 18:10:25 +0900

    BOTの投稿は編集できなくした

--- H-I-D/web-client/web/js/message.js
+++ H-I-D/web-client/web/js/message.js
@@ -24,24 +24,18 @@ function appendMessages(data) {
  */
 function appendMessage(message) {
     var messageHTML = "";
-    var escapeBody = "http://farm6.staticflickr.com/5613/15634745431_af629b9374.jpg";
+    //var escapeBody = "http://farm6.staticflickr.com/5613/15634745431_af629b9374.jpg";
     //var escapeBody = "aaaaaa";
-    //var escapeBody = $("<div/>").text(message.body).html();
+    var escapeBody = $("<div/>").text(message.body).html();
     var escapeDate = $("<div/>").text(message.created_at).html();
-    var escapeUser = $("<div/>").text(message.user_name).html();
 
     if(escapeBody.match(/(http|ftp):\/\/.+/))
     {
         messageHTML += 
             '<div class="media">' +
                 '<div class="media-body">' +
-                    '<span class="media-message-name">' + escapeUser + '</span>  ' +
                     '<span class="media-message-date">' + escapeDate + '</span>' + '<br>' +
                     '<span class="media-message-body"><a href="' + escapeBody + '"><img src="' + escapeBody + '" alt="image"></img></a></span>' +
-                '</div>' +
-                '<div class="media-right">' +
-                    '<button type="button" class="pull-right btn btn-default btn-xs" data-toggle="modal" data-target="#edit-modal" data-body="' + escapeBody + '" data-id="' + message.id +'">' +
-                    '<span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span>' +
                 '</div>' +
             '</div>' +
             '<hr>';
@@ -51,7 +45,6 @@ function appendMessage(message) {
         messageHTML +=
             '<div class="media">' +
                 '<div class="media-body">' +
-                    '<span class="media-message-name">' + escapeUser + '</span>  ' +
                     '<span class="media-message-date">' + escapeDate + '</span>' + '<br>' +
                     '<span class="media-message-body">' + escapeBody + '</span>' +
                 '</div>' +
