c83d1073fd61e315533a8b9fab1f8df80dd834ab
saitoshoma <saitoshoma@saitoshoma-no-MacBook-Air.local>
2016-07-10 14:20:32 +0900

    サーバ側の実装は終わりました

--- tantakan/server/src/model/message.go
+++ tantakan/server/src/model/message.go
@@ -15,9 +15,8 @@ type (
 	Message struct {
 		Id   int    `json:"id"`
 		Body string `json:"body"`
-		// CreatedAt string `json:"created_at"` // 1-1. メッセージの投稿時刻
 		CreatedAt string `json:"created_at"`
-		// Username  string `json:"user_name"`  // 1-2. ユーザ名
+		Username  string `json:"user_name"`
 	}
 	Messages []Message
 )
@@ -94,7 +93,7 @@ func deleteMessageId(id int) error {
 
 // メッセージをつくる
 // 1-2. ユーザ名を受け取ってメッセージをつくる
-func NewMessage(body string) (*Message, error) {
+func NewMessage(body,username string) (*Message, error) {
 	id, err := newMessageId()
 	if err != nil {
 		return nil, err
@@ -103,10 +102,8 @@ func NewMessage(body string) (*Message, error) {
 	return &Message{
 		Id:   id,
 		Body: body,
-		// 1-1. CreatedAt に時刻をセットする
-		// ヒント: https://golang.org/pkg/time/
 		CreatedAt: t,
-		// 1-2. Username にユーザ名をセットする
+		Username: username,
 	}, nil
 }
 
