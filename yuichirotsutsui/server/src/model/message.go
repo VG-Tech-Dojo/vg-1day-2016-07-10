11315b6555a95687bd52da7ba85bb8d04928b6fb
tsutsui <tsutsui@tsutsui-no-MacBook-Air.local>
2016-07-10 13:32:15 +0900

    create mission-1-1

--- yuichirotsutsui/server/src/model/message.go
+++ yuichirotsutsui/server/src/model/message.go
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"sort"
-
+    "time"
 	"github.com/pkg/errors"
 )
 
@@ -14,7 +14,7 @@ type (
 	Message struct {
 		Id   int    `json:"id"`
 		Body string `json:"body"`
-		// CreatedAt string `json:"created_at"` // 1-1. メッセージの投稿時刻
+		CreatedAt string `json:"created_at"` // 1-1. メッセージの投稿時刻
 		// Username  string `json:"user_name"`  // 1-2. ユーザ名
 	}
 	Messages []Message
@@ -101,6 +101,7 @@ func NewMessage(body string) (*Message, error) {
 	return &Message{
 		Id:   id,
 		Body: body,
+        CreatedAt: (time.Now()).String(),
 		// 1-1. CreatedAt に時刻をセットする
 		// ヒント: https://golang.org/pkg/time/
 		// 1-2. Username にユーザ名をセットする
