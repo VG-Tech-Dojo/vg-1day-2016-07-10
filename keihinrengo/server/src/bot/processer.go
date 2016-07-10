2e7164d7f226dee9a1a35f440557288b5954beff
kuro <mhgeoe@gmail.com>
2016-07-10 17:47:15 +0900

    途中経過

--- keihinrengo/server/src/bot/processer.go
+++ keihinrengo/server/src/bot/processer.go
@@ -2,11 +2,13 @@ package bot
 
 import (
 	"fmt"
-	"time"
 	"math/rand"
-	"github.com/ChimeraCoder/anaconda"
 	"model"
 	"net/url"
+	"strings"
+	"time"
+
+	"github.com/ChimeraCoder/anaconda"
 )
 
 type (
@@ -28,13 +30,20 @@ type (
 	}
 
 	UranaiProcesser struct {
-
 	}
 
 	// TimelineProcesser
 	// homeのtimelineのtweetを1つ取得するProcesser
 	TimelineProcesser struct {
 		Api *anaconda.TwitterApi
+	}
+
+	WordList struct {
+		Words []string
+	}
+
+	ShiritoriProcesser struct {
+		Dict map[string]WordList
 	}
 )
 
@@ -51,27 +60,20 @@ func (p *TimelineProcesser) Init() {
 	p.Api = getTwitterApi()
 }
 
-func (*UranaiProcesser ) Process(msgIn *model.Message) *model.Message {
+func (*UranaiProcesser) Process(msgIn *model.Message) *model.Message {
 	rand.Seed(time.Now().UnixNano())
 	random := rand.Intn(3)
-	text:= ""
+	text := ""
 	switch random {
-	case 0: text = "大吉"
-	case 1: text = "吉"
-	case 2: text = "凶"
+	case 0:
+		text = "大吉"
+	case 1:
+		text = "吉"
+	case 2:
+		text = "凶"
 	}
 	return &model.Message{Body: text}
 }
-
-// func (*WarikanProcesser ) Process(msgIn *model.Message) *model.Message {
-// 	var num int = 1
-// 	text := msgIn.Body
-// 	texts := Split(text, " " string)
-// 	for i:=1; i< len(texts);i+=1{
-//
-// 	}
-// 	return &model.Message{Body: text}
-// }
 
 func (p *TimelineProcesser) Process(msgIn *model.Message) *model.Message {
 	if p.Api == nil {
@@ -87,4 +89,25 @@ func (p *TimelineProcesser) Process(msgIn *model.Message) *model.Message {
 
 	tweet := timeline[0]
 	return &model.Message{Body: fmt.Sprintf("[timeline:%s] %s", tweet.User.Name, tweet.Text)}
+}
+
+func (p *ShiritoriProcesser) Init() {
+	p.Dict = map[string]string{
+		"あ": Wordlist{
+			"words": {
+				"あんこ",
+			},
+		},
+	}
+}
+
+func (p *ShiritoriProcesser) Process(msgIn *model.Message) *model.Message {
+	s := strings.Fields(msgIn.Body)
+	head := s[1][0:3]
+	if candidates, ok := p.Dict[head]; ok {
+		ans := candidates.words[rand.Intn(len(candidates))]
+		return &model.Message{Body: ans}
+	} else {
+		return &model.Message{Body: "参りました"}
+	}
 }
