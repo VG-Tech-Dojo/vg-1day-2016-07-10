211f134e51d3b27a2d9c956652ce094a5d9fea5b
Hideaki Kanehara <sss_ftr.hid@icloud.com>
2016-07-10 18:12:39 +0900

    キーワードから画像を取得

--- H-I-D/server/src/bot/processer.go
+++ H-I-D/server/src/bot/processer.go
@@ -8,20 +8,14 @@ import (
 	"math/rand"
 	"time"
 	"strings"
-<<<<<<< HEAD
-	// "strconv"
-	// "encoding/json"
-	// "errors"
-	// "io"
-	// "io/ioutil"
-	// "net/http"
-	// "os"
-	// "runtime"
-	// "strings"
-	// "github.com/jmoiron/jsonq"
-=======
 	"strconv"
->>>>>>> 019341f0835a826e48259937ef6e0a3844a0c64d
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"runtime"
+	"github.com/jmoiron/jsonq"
 )
 
 type (
@@ -118,7 +112,110 @@ func (p *TimelineProcesser) Process(msgIn *model.Message) *model.Message {
 func (p *ImageProcesser) Process(msgIn *model.Message) *model.Message {
 	inputs := strings.Split(msgIn.Body, " ")
 	query := inputs[1]
-	imageurl := "https://pbs.twimg.com/profile_images/459921170251264000/ax4FMwXA.jpeg"
+	imageurl := Get(query);
 	txt := imageurl + query
 	return &model.Message{Body: txt}
+}
+
+
+/* imagebot
+-------------------*/
+func getAPIKey() (apikey string, err error) {
+	apikey = os.Getenv("BING_API_KEY")
+	if apikey == "" {
+		return "", errors.New("Not Set APIKEY")
+	}
+	return
+}
+
+func getImageType(contentType string) (imageType string, err error) {
+	switch contentType {
+	case "image/jpeg":
+		imageType = "jpeg"
+	case "image/png":
+		imageType = "png"
+	case "image/gif":
+		imageType = "gif"
+	default:
+		return "", errors.New("Unknown ContentType")
+	}
+
+	return
+}
+
+func getJSON(query string) (json string, err error) {
+	client := &http.Client{}
+	URL := "https://api.datamarket.azure.com/Bing/Search/Image"
+	apikey, err := getAPIKey()
+	if err != nil {
+		return "", err
+	}
+
+	values := url.Values{}
+	values.Add("Query", query)
+	values.Add("$format", "json")
+	req, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.URL.RawQuery = values.Encode()
+	req.SetBasicAuth(apikey, apikey)
+	response, _ := client.Do(req)
+	body, _ := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+
+	json = string(body)
+
+	return
+}
+
+func parseJSON(jsonStr string) (urls [][]string, err error) {
+
+	data := map[string]interface{}{}
+	dec := json.NewDecoder(strings.NewReader(jsonStr))
+	dec.Decode(&data)
+	jq := jsonq.NewQuery(data)
+
+	for i := 0; i < 1; i++ {
+		url, _ := jq.String("d", "results", strconv.Itoa(i), "MediaUrl")
+		contentType, _ := jq.String("d", "results", strconv.Itoa(i), "ContentType")
+		imageType, err := getImageType(contentType)
+		if err != nil {
+			return nil, err
+		}
+
+		urls = append(urls, []string{url, imageType})
+	}
+	return
+}
+
+func Get(query string) string {
+
+	jsonStr, err := getJSON(query)
+	if err != nil {
+		panic(err)
+	}
+	urls, err := parseJSON(jsonStr)
+	if err != nil {
+		panic(err)
+	}
+
+	timeStamp := time.Now().Format("20060102150405")
+
+	dirName := "mimorin-" + timeStamp
+	if err := os.Mkdir(dirName, 0777); err != nil {
+		panic(err)
+	}
+	cpus := runtime.NumCPU()
+	runtime.GOMAXPROCS(cpus)
+
+	statusChan := make(chan string)
+	for idx, url := range urls {
+		filePath := dirName + "/" + "mimorin" + strconv.Itoa(idx) + "." + url[1]
+		go func(url, filePath string) {
+			statusChan <- (url)
+		}(url[0], filePath)
+	}
+	return <-statusChan
 }
